1217ccb21834c8eb0642256bd21eac64185c5832
DAW208 <daw08.2024@gmail.com>
2024-10-10 12:01:55 +0200

    Mas

--- 1EVAL/TEMA2/CuartoDocumento/Ejemplo5EnAdelante.php
+++ 1EVAL/TEMA2/CuartoDocumento/Ejemplo5EnAdelante.php
@@ -50,4 +50,46 @@ echo "<br>";
 print_r($family= array("Fred", "Wilma", "Pebbles"));
 echo "<br>";
 print_r($size= count($family)); // Cuenta el numero de valores dentro de un array
+//Rellenando un array
+print_r($scores= array(5, 10));
+print_r($padded= array_pad($scores, 5, 0));
+print_r($padded2= array_pad($scores, -5, 0));
+echo "<br>";
+//10
+// FIRST EXERCISE WITH ARRAYS
+$a= $arrayName= array(8, 9);
+foreach ($a as $value) {
+    echo ($value."<br>");
+}
+//11
+$row0= array(1, 2, 3);
+$row1= array(4, 5, 6);
+$row2= array(7, 8, 9);
+$multi= array($row0, $row1, $row2);
+$value= $multi[2][0]; //Row 2, column 0. $value= 7
+echo("The value at row 2, column 0 is {$value}<br>");
+//12
+$person= array("Fred", 35, "Betty");
+list($name, $age, $wife)= $person;
+// $name is "Fred", $age is 35, $wife is "Betty"
+echo "<br>";
+//13
+print_r($people= array("Tom", "Dick", "Harriet", "Brenda", "Jo"));
+$middle= array_slice($people, 2, 2); // $middle is array("Harriet", "Brenda")
+echo "<br>";
+// Exercise 2.11
+print_r("Ejercicio 11");
+print_r($second= array_slice($people, 2));
+print_r($third= array_slice($people, 3));
+echo "<br>";
+//14
+print_r($person= array('name'=> "Fred", 'age' => 35, 'wife' => "Wilma"));
+print_r($keys= array_keys($person)); //$Keys is array("name", "age", "wife")
+print_r($values= array_values($person));
+echo "<br>";
+//15
 
+$person['age'] = 0;
+if (array_key_exists('age', $person)) {
+    echo "exist!";
+}
